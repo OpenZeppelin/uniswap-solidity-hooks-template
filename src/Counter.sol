70f552546551a6da0763d98c4ca298d033160404
Daniel Bigos <daniel.bigos@icloud.com>
2024-11-14 20:04:04 +0100

    feat: add call to custom curve contract

--- src/Counter.sol
+++ src/Counter.sol
@@ -8,7 +8,9 @@ import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
-import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary, toBeforeSwapDelta} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {Currency, CurrencyLibrary} from "v4-core/src/types/Currency.sol";
+import {SafeCast} from "v4-core/src/libraries/SafeCast.sol";
 
 interface IUniswapCurve {
     function getAmountInForExactOutput(uint256 amountOut, address input, address output, bool zeroForOne)
@@ -22,6 +24,8 @@ interface IUniswapCurve {
 
 contract Counter is BaseHook {
     using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+    using SafeCast for uint256;
 
     // NOTE: ---------------------------------------------------------
     // state variables should typically be unique to a pool
@@ -34,10 +38,10 @@ contract Counter is BaseHook {
     mapping(PoolId => uint256 count) public beforeAddLiquidityCount;
     mapping(PoolId => uint256 count) public beforeRemoveLiquidityCount;
 
-    IUniswapCurve _customCurveContract;
+    IUniswapCurve _curveContract;
 
-    constructor(IPoolManager _poolManager, address _customCurveAddress) BaseHook(_poolManager) {
-        _customCurveContract = IUniswapCurve(_customCurveAddress);
+    constructor(IPoolManager _poolManager, address _curveContractAddress) BaseHook(_poolManager) {
+        _curveContract = IUniswapCurve(_curveContractAddress);
     }
 
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
@@ -63,13 +67,40 @@ contract Counter is BaseHook {
     // NOTE: see IHooks.sol for function documentation
     // -----------------------------------------------
 
-    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata)
+    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata)
         external
         override
         returns (bytes4, BeforeSwapDelta, uint24)
     {
+        bool exactInput = params.amountSpecified < 0;
+        (Currency specified, Currency unspecified) =
+            (params.zeroForOne == exactInput) ? (key.currency0, key.currency1) : (key.currency1, key.currency0);
+
+        uint256 specifiedAmount = exactInput ? uint256(-params.amountSpecified) : uint256(params.amountSpecified);
+        uint256 unspecifiedAmount;
+
+        BeforeSwapDelta returnDelta;
+
+        if (exactInput) {
+            unspecifiedAmount = _curveContract.getAmountOutFromExactInput(
+                specifiedAmount, Currency.unwrap(specified), Currency.unwrap(unspecified), params.zeroForOne
+            );
+            /*
+             * Additional logic here if needed.
+            */
+            returnDelta = toBeforeSwapDelta(specifiedAmount.toInt128(), -unspecifiedAmount.toInt128());
+        } else {
+            unspecifiedAmount = _curveContract.getAmountInForExactOutput(
+                specifiedAmount, Currency.unwrap(unspecified), Currency.unwrap(specified), params.zeroForOne
+            );
+            /*
+             * Additional logic here if needed.
+            */
+            returnDelta = toBeforeSwapDelta(-specifiedAmount.toInt128(), unspecifiedAmount.toInt128());
+        }
+
         beforeSwapCount[key.toId()]++;
-        return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+        return (BaseHook.beforeSwap.selector, returnDelta, 0);
     }
 
     function afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata)
--- test/MockCurve.sol
+++ test/MockCurve.sol
@@ -1,21 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {Currency} from "v4-core/src/types/Currency.sol";
+
 contract MockCurve {
-    function getAmountInForExactOutput(uint256 amountOut, address input, address output, bool zeroForOne)
-        external
-        pure
-        returns (uint256)
-    {
+    function getAmountInForExactOutput(uint256 amountOut, address, address, bool) external pure returns (uint256) {
+        // in constant-sum curve, tokens trade exactly 1:1
         uint256 amountIn = amountOut;
         return amountIn;
     }
 
-    function getAmountOutFromExactInput(uint256 amountIn, address input, address output, bool zeroForOne)
-        external
-        pure
-        returns (uint256)
-    {
+    function getAmountOutFromExactInput(uint256 amountIn, address, address, bool) external pure returns (uint256) {
+        // in constant-sum curve, tokens trade exactly 1:1
         uint256 amountOut = amountIn;
         return amountOut;
     }
